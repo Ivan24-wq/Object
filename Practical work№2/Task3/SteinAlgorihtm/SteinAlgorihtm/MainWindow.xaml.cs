c63aa6402dca7f1fba87cec7e3817f2ed1fadb78
Ivan24-wq <iv8747583@gmail.com>
2025-10-13 21:50:30 +0300

    code

---
+++ Practical work№2/Task3/SteinAlgorihtm/SteinAlgorihtm/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SteinAlgorihtm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void num_Click(object sender, RoutedEventArgs e)
+        {
+            string input = input_num.Text;
+            if(input.Contains('.') || input.Contains(','))
+            {
+                output.Text = "Ошибка нужно вводить только целын числа";
+                return;
+            }
+            //Проверка на целочисленность
+            if(!int.TryParse(input, out int number))
+            {
+                output.Text = "Нужно вводить только целые числа!";
+                return;
+            }
+
+            string result = Prime.ShowShift(number);
+            output.Text = result;
+        }
+    }
+}
---
+++ Practical work№2/Task3/SteinAlgorihtm/SteinAlgorihtm/Prime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteinAlgorihtm
+{
+    class Prime
+    {
+        private static bool IsPrime(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i * i <= n; ++i)
+                if (n % i == 0) return false;
+            return true;
+        }
+
+        //Представим число через двойной сдвиг
+        private static string Shift(int number)
+        {
+            return number switch
+            {
+                11 => "(2 << 3) + (2 << 1) + (2>>1)",
+                7 => "(2 << 2) + (2 << 1) - (2 >> 1)",
+                5 => "(2 << 2) + (2 >> 1)",
+                3 => "2 + (2 >> 1)",
+                2 => "2",
+                _ => "Нет представленного типа"
+            };
+        }
+
+        public static string ShowShift(int n)
+        {
+            int prime = n - 1;
+            while (prime > 1  && !IsPrime(prime))
+                prime--;
+            string res = $"Наибольшее простое: {n}:{prime}\n" +
+                         $"Представленное через 2 сдвига: {Shift(prime)}";
+            return res;
+        }
+    }
+}
