5ba3f77c834ccece9be2222f20466eff1206f059
Ivan24-wq <iv8747583@gmail.com>
2025-10-26 21:40:09 +0300

    class Switch

---
+++ Practical work№3/Task1/Switch.cs
@@ -0,0 +1,21 @@
+﻿namespace Task1;
+
+public class Switch
+{
+    public enum CoolantSystemStatus { OK, Check, Fail }
+    public enum SuccessFailureResult { Success, Fail }
+
+    public class Switch
+    {
+        private Random rand = new Random();
+
+        public SuccessFailureResult DisconnectPowerGenerator()
+        {
+            SuccessFailureResult r = SuccessFailureResult.Fail;
+            if (rand.Next(1, 10) > 2) r = SuccessFailureResult.Success;
+            if (rand.Next(1, 20) > 18) throw new
+PowerGeneratorCommsException("Network failure accessing Power Generator monitoringsystem");
+            return r;
+        }
+    }
+}
