7661dba4548541f0b00e22bd4bbaeecf35788e61
Ivan24-wq <iv8747583@gmail.com>
2025-10-27 11:51:00 +0300

    Class Switch

--- Practical work№3/Task1/Switch.cs
+++ Practical work№3/Task1/Switch.cs
@@ -1,21 +1,153 @@
 ﻿namespace Task1;
 
+public enum CoolantSystemStatus { OK, Check, Fail }
+
+public enum SuccessFailureResult { Success, Fail }
+
 public class Switch
 {
-    public enum CoolantSystemStatus { OK, Check, Fail }
-    public enum SuccessFailureResult { Success, Fail }
+    private Random rand = new Random();
 
-    public class Switch
+    // Отключение от внешней системы генерации электроэнергии
+    public SuccessFailureResult DisconnectPowerGenerator()
     {
-        private Random rand = new Random();
+        SuccessFailureResult r = SuccessFailureResult.Fail;
 
-        public SuccessFailureResult DisconnectPowerGenerator()
-        {
-            SuccessFailureResult r = SuccessFailureResult.Fail;
-            if (rand.Next(1, 10) > 2) r = SuccessFailureResult.Success;
-            if (rand.Next(1, 20) > 18) throw new
-PowerGeneratorCommsException("Network failure accessing Power Generator monitoringsystem");
-            return r;
-        }
+        if (rand.Next(1, 10) > 2)
+            r = SuccessFailureResult.Success;
+
+        if (rand.Next(1, 20) > 18)
+            throw new PowerGeneratorCommsException("Сбой сети при доступе к системе мониторинга генератора");
+
+        return r;
     }
+
+    // Проверка основной системы охлаждения
+    public CoolantSystemStatus VerifyPrimaryCoolantSystem()
+    {
+        CoolantSystemStatus c = CoolantSystemStatus.Fail;
+        int r = rand.Next(1, 10);
+
+        if (r > 5)
+        {
+            c = CoolantSystemStatus.OK;
+        }
+        else if (r > 2)
+        {
+            c = CoolantSystemStatus.Check;
+        }
+
+        if (rand.Next(1, 20) > 18)
+            throw new CoolantTemperatureReadException("Ошибка при чтении температуры основной системы охлаждения");
+
+        if (rand.Next(1, 20) > 18)
+            throw new CoolantPressureReadException("Ошибка при чтении давления основной системы охлаждения");
+
+        return c;
+    }
+
+    // Проверка резервной системы охлаждения
+    public CoolantSystemStatus VerifyBackupCoolantSystem()
+    {
+        CoolantSystemStatus c = CoolantSystemStatus.Fail;
+        int r = rand.Next(1, 10);
+
+        if (r > 5)
+        {
+            c = CoolantSystemStatus.OK;
+        }
+        else if (r > 2)
+        {
+            c = CoolantSystemStatus.Check;
+        }
+
+        if (rand.Next(1, 20) > 19)
+            throw new CoolantTemperatureReadException("Ошибка при чтении температуры резервной системы охлаждения");
+
+        if (rand.Next(1, 20) > 19)
+            throw new CoolantPressureReadException("Ошибка при чтении давления резервной системы охлаждения");
+
+        return c;
+    }
+
+    // Получение температуры реакторного ядра
+    public double GetCoreTemperature()
+    {
+        if (rand.Next(1, 20) > 18)
+            throw new CoreTemperatureReadException("Ошибка при чтении температуры реакторного ядра");
+
+        return rand.NextDouble() * 1000;
+    }
+
+    // Вставка стержней для остановки реакции
+    public SuccessFailureResult InsertRodCluster()
+    {
+        SuccessFailureResult r = SuccessFailureResult.Fail;
+
+        if (rand.Next(1, 100) > 5)
+            r = SuccessFailureResult.Success;
+
+        if (rand.Next(1, 10) > 8)
+            throw new RodClusterReleaseException("Сбой сенсора: невозможно подтвердить выпуск стержней");
+
+        return r;
+    }
+
+    // Получение уровня радиации реакторного ядра
+    public double GetRadiationLevel()
+    {
+        if (rand.Next(1, 20) > 18)
+            throw new CoreRadiationLevelReadException("Ошибка при чтении уровня радиации реакторного ядра");
+
+        return rand.NextDouble() * 500;
+    }
+
+    // Отправка сообщения о завершении останова реактора
+    public void SignalShutdownComplete()
+    {
+        if (rand.Next(1, 20) > 18)
+            throw new SignallingException("Сбой сети при подключении к системе оповещения");
+    }
+}
+
+// ---------------- Исключения ----------------
+
+public class PowerGeneratorCommsException : Exception
+{
+    public PowerGeneratorCommsException(string message) : base(message) { }
+}
+
+public class CoolantSystemException : Exception
+{
+    public CoolantSystemException(string message) : base(message) { }
+}
+
+public class CoolantTemperatureReadException : CoolantSystemException
+{
+    public CoolantTemperatureReadException(string message) : base(message) { }
+}
+
+public class CoolantPressureReadException : CoolantSystemException
+{
+    public CoolantPressureReadException(string message) : base(message) { }
+}
+
+public class CoreTemperatureReadException : Exception
+{
+    public CoreTemperatureReadException(string message) : base(message) { }
+}
+
+public class CoreRadiationLevelReadException : Exception
+{
+    public CoreRadiationLevelReadException(string message) : base(message) { }
+}
+
+public class RodClusterReleaseException : Exception
+{
+    public RodClusterReleaseException(string message) : base(message) { }
+}
+
+public class SignallingException : Exception
+{
+    public SignallingException(string message) : base(message) { }
 }
---
+++ Practical work№3/TestTask/Program.cs
@@ -0,0 +1,41 @@
+﻿int x, y, i;
+i = int.Parse(Console.ReadLine()!);
+try
+{
+    try
+    {
+        x = 5;
+        y = x / i;
+        Console.WriteLine($"x = {x}, y = {y}", x, y);
+    }
+    catch (System.FormatException e)
+    {
+        Console.WriteLine("Введено не целое число! Исключение", e.ToString());
+    }
+    /*
+        catch (System.DivideByZeroException e)
+        {
+            Console.WriteLine("Попытка деления на ноль" + e.ToString());
+        }
+    */
+
+    catch
+    {
+        Console.WriteLine("Неизвестная ошибка. Перезапустите программу!");
+        throw;
+    }
+    finally
+    {
+        Console.WriteLine("Выполни блок finally");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+    Console.WriteLine(ex.TargetSite);
+    Console.WriteLine(ex.InnerException);
+    Console.WriteLine(ex.Source);
+    Console.WriteLine(ex.Data);
+    Console.WriteLine(ex.HelpLink);
+}
