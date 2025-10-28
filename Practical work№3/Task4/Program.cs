3aabc3503dc07ad5b378e02287b3761081d3a9f8
Ivan24-wq <iv8747583@gmail.com>
2025-10-28 18:29:42 +0300

    Task3

---
+++ Practical work№3/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 2147483647;
+int b = 2;
+
+//Исключение на переполнение типа
+try
+{
+    int res = checked(a * b);
+    Console.WriteLine(res);
+}
+catch(OverflowException)
+{
+    Console.WriteLine("Ошибка! Переполнение типа int!");
+}
