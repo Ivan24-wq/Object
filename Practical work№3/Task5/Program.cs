117bfe5e266830284f0a1d1c356c8e219904106a
Ivan24-wq <iv8747583@gmail.com>
2025-10-28 19:38:31 +0300

    Code

---
+++ Practical work№3/Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+//Строка Имя Фамилия
+string fio = "Воронин Иван Дмитриевич";
+//Преобразование строки в массив байтов
+byte[] inputBytes = Encoding.UTF8.GetBytes(fio);
+
+
+//Для алгоритма  SHA-256(вычисляем хэш)
+using (SHA256 sha256 = SHA256.Create())
+{
+    byte[] hashBytes = sha256.ComputeHash(inputBytes);
+    string hashHex = Convert.ToHexString(hashBytes);
+    Console.WriteLine($"ФИО: {fio}");
+    Console.WriteLine($"ХЭШ ФИО: {hashHex}");
+}
