1c1326b04d3f4dd8db9bc3d6087f60351a5c6554
Ivan24-wq <iv8747583@gmail.com>
2025-11-08 20:17:15 +0300

    code

--- Practical work№4/Task1/Complexx.cs
+++ Practical work№4/Task1/Complexx.cs
@@ -3,56 +3,56 @@ using System.Reflection;
 
 namespace Task1;
 
-public struct Complex
+public struct Complexx
 {
     //Поля структуры
     public double Real { get; } //действиетельная часть
     public double Imag { get; } //мнимая часть
 
     //Конструктор
-    public Complex(double real, double imag)
+    public Complexx(double real, double imag)
     {
         Real = real;
         Imag = imag;
     }
 
     //Частные случаии(например 1 + 0i)
-    public static Complex One => new Complex(1, 0);
-    public static Complex Zero => new Complex(0, 0);
-    public static Complex ImagZero => new Complex(0, 1); // 0+1i
-    public static Complex MinusOne => new Complex(-1, 0);
-    public static Complex ImagMinusOne => new Complex(0, -1);
+    public static Complexx One => new Complexx(1, 0);
+    public static Complexx Zero => new Complexx(0, 0);
+    public static Complexx ImagZero => new Complexx(0, 1); // 0+1i
+    public static Complexx MinusOne => new Complexx(-1, 0);
+    public static Complexx ImagMinusOne => new Complexx(0, -1);
 
     // ----------------Свойства-------------------------
 
     //Сложение
-    public static Complex operator +(Complex a, Complex b)
+    public static Complexx operator +(Complexx a, Complexx b)
     {
-        return new Complex(a.Real + b.Imag, a.Imag + b.Imag);
+        return new Complexx(a.Real + b.Real, a.Imag + b.Imag);
     }
 
     //Вычитание
-    public static Complex operator -(Complex a, Complex b)
+    public static Complexx operator -(Complexx a, Complexx b)
     {
-        return new Complex(a.Real - b.Imag, a.Imag - b.Imag);
+        return new Complexx(a.Real - b.Real, a.Imag - b.Imag);
     }
 
     //Умножение
-    public static Complex operator *(Complex a, Complex b)
+    public static Complexx operator *(Complexx a, Complexx b)
     {
-        return new Complex(a.Real * b.Real - a.Imag - b.Imag,
+        return new Complexx(a.Real * b.Real - a.Imag * b.Imag,
         a.Imag * b.Real + a.Real * b.Imag);
     }
 
     //деление
-    public static Complex operator /(Complex a, Complex b)
+    public static Complexx operator /(Complexx a, Complexx b)
     {
         double denominator = b.Real * b.Real + b.Imag * b.Imag;
         if (denominator == 0)
         {
             throw new DivideByZeroException("Нельзя делить на ноль!");
         }
-        return new Complex((a.Real * b.Real + a.Imag * b.Imag) / denominator,
+        return new Complexx((a.Real * b.Real + a.Imag * b.Imag) / denominator,
         (a.Imag * b.Real - a.Real * b.Imag) / denominator);
     }
 
@@ -63,33 +63,33 @@ public struct Complex
     //преобразование в экспотенциальную форму
     public (double r, double fi) ToExpotential()
     {
-        double r = Math.Sqrt(Real * Real + Imag * IMag);
+        double r = Math.Sqrt(Real * Real + Imag * Imag);
         double fi = Math.Atan2(Imag, Real);
         return (r, fi);
     }
 
     //обратное преобразование
-    public static Complex FromExpotential(double r, double fi)
+    public static Complexx FromExpotential(double r, double fi)
     {
         double real = r * Math.Cos(fi);
         double imag = r * Math.Sin(fi);
-        return new Complex(real, imag);
+        return new Complexx(real, imag);
     }
 
     //Перегрузка оператора ==
-    public static bool operator ==(Complex a, Complex b)
+    public static bool operator ==(Complexx a, Complexx b)
     {
         return Math.Abs(a.Real - b.Real) < 1e-10 &&
         Math.Abs(a.Imag - b.Imag) < 1e-10;
     }
 
     //Перегрузка !=
-    public static bool operator !=(Complex a, Complex b) => (a == b);
+    public static bool operator !=(Complexx a, Complexx b) => !(a == b);
 
     //Пререопределение Equals
     public override bool Equals(object? obj)
     {
-        if (obj is Complex c)
+        if (obj is Complexx c)
         {
             return this == c;
         }
@@ -107,9 +107,9 @@ public struct Complex
         if (Imag == 0)
             return $"{Real}";
         if (Real == 0)
-            return $"{Imag}";
+            return $"{Imag}i";
         if (Imag > 0)
-            return $"{Real} + {Imag}";
-        return $"{Real}"
+            return $"{Real} + {Imag}i";
+        return $"{Real} - {Imag}i";
     }
 }
--- Practical work№4/Task1/Program.cs
+++ Practical work№4/Task1/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿namespace Task1;
+class Program
+{
+    static void Main()
+    {
+        var a = new Complexx(2, 1);
+        var b = new Complexx(3, -1);
+
+        //Мат опреции
+        var add = a + b;
+        var minus = a - b;
+        var multiply = a * b;
+        var div = a / b;
+        var mod = a.Module;
+
+
+        //Вывод
+        Console.WriteLine($"\nСложение чисел: (2+i)*(3-i): {add}");
+        Console.WriteLine($"\nВычитание: (2+i)*(3-i): {minus}");
+        Console.WriteLine($"\nУмножение: (2+i)*(3-i): {multiply}");
+        Console.WriteLine($"\nДеление: (2+i)*(3-i): {div}");
+        Console.WriteLine($"\nМодуль числа: (2+i): {mod:F3}");
+    }
+}
