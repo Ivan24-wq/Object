beec2f7fcf769b9c33d4c0db09cbeca0bd8eb431
Ivan24-wq <iv8747583@gmail.com>
2025-11-24 15:42:26 +0300

    Интерфейс, который принимает величины в метричейской системе и конвертирует в империческую и наоборот

---
+++ Practical work№5/Task1/IMeasuringDevice.cs
@@ -0,0 +1,10 @@
+namespace MeasuringDevice;
+
+public interface IMeasuringDevice
+{
+    decimal MetricValue();
+    decimal ImperialValue();
+    void StartCollecting();
+    void StopCollecting();
+    int[] GetRawData();
+}
